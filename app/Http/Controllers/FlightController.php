ae105c91e2ee765e6af9fe806db579182c2a4cd5
Danilo D Vaz <danilodvaz@gmail.com>
2020-11-01 15:25:52 -0300

    Finalizado processo de agrupamento de voos.

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -16,12 +16,9 @@ class FlightController extends Controller
 		$outboundFlights = $this->getFlights(self::OUTBOUND_FLIGHTS);
 		$inboundFlights = $this->getFlights(self::INBOUND_FLIGHTS);
 
-		$outboundFlightsPrepared = $this->getFlightsPreparedArray($outboundFlights);
-		$inboundFlightsPrepared = $this->getFlightsPreparedArray($inboundFlights);
+		$flightGrouping = $this->getFlightGrouping($outboundFlights, $inboundFlights);
 
-		$this->getFlightGrouping($outboundFlightsPrepared, $inboundFlightsPrepared);
-
-    	// return $group;
+		return response()->json($flightGrouping);
 	}
 	
 	private function getFlights($filter = null)
@@ -43,44 +40,116 @@ class FlightController extends Controller
 		$fareGroup = array();
 
 		foreach ($flights as $flight) {
-			$fareGroup[$flight['fare']][$flight['price']][] = $flight;
+			$fareGroup[$flight['fare']][$flight['price']][] = array('id' => $flight['id']);
 		}
 
 		return $fareGroup;
 	}
 
-	private function getFlightGrouping($outboundFlightsPrepared, $inboundFlightsPrepared)
+	private function getFlightGrouping($outboundFlights, $inboundFlights)
 	{
-		$flightGrouping = array();
+		$flights = array_merge($outboundFlights, $inboundFlights);
+
+		$outboundFlightsPrepared = $this->getFlightsPreparedArray($outboundFlights);
+		$inboundFlightsPrepared = $this->getFlightsPreparedArray($inboundFlights);
+
+		$grouping = $this->buildGrouping($outboundFlightsPrepared, $inboundFlightsPrepared);
+		$flightGrouping = $this->buildFlightGrouping($flights, $grouping);
+
+		return $flightGrouping;
+	}
+
+	private function buildGrouping($outboundFlightsPrepared, $inboundFlightsPrepared)
+	{
+		$grouping = array();
+		$uniqueId = 0;
 
 		foreach ($outboundFlightsPrepared as $fare => $outboundFlightsPrices) {
 			$inboundFlightsPrices = $inboundFlightsPrepared[$fare];
 
 			if (!empty($outboundFlightsPrices) && !empty($inboundFlightsPrices)) {
-				$this->buildFlightGrouping($outboundFlightsPrices, $inboundFlightsPrices);
+				$groupingFare = $this->buildGroupingFare($uniqueId, $outboundFlightsPrices, $inboundFlightsPrices);
+				$grouping = array_merge($grouping, $groupingFare);
 			}
 		}
+
+		usort($grouping, function($groupA, $groupB) { return ($groupA['totalPrice'] <=> $groupB['totalPrice']); });		
+
+		return $grouping;
 	}
 
-	private function buildFlightGrouping($outboundFlights, $inboundFlights)
+	private function buildGroupingFare(&$uniqueId, $outboundFlights, $inboundFlights)
 	{
+		$groupingFare = array();
 		$outboundFlightsPrices = array_keys($outboundFlights);
 		$inboundFlightsPrices = array_keys($inboundFlights);
 
 		foreach ($outboundFlightsPrices as $outboundPrice) {
 			foreach ($inboundFlightsPrices as $inboundPrice) {
-				// Criar o grupo para cada combinação;
+				$grouping = $this->groupingPattern();
+
+				$grouping['uniqueId'] = ++$uniqueId;
+				$grouping['totalPrice'] = $outboundPrice + $inboundPrice;
+				$grouping['outbound'] = $outboundFlights[$outboundPrice];
+				$grouping['inbound'] = $inboundFlights[$inboundPrice];
+
+				$groupingFare[] = $grouping;
 			}
 		}
+
+		return $groupingFare;
 	}
 
-	private function patternGroup()
+	private function groupingPattern()
 	{
 		return array(
 			"uniqueId" => 0,
 			"totalPrice" => 0,
-			"outBound" => array(),
+			"outbound" => array(),
 			"inbound" => array()
 		);
 	}
+
+	private function buildFlightGrouping($flights, $grouping)
+	{
+		$flightGrouping = $this->flightGroupingPattern();
+
+		$flightGrouping['flights'] = $flights;
+		$flightGrouping['groups'] = $grouping;
+		$flightGrouping['totalGroups'] = count($grouping);
+		$flightGrouping['totalFlights'] = $this->getTotalUsedFlights($grouping);
+		$flightGrouping['cheapestPrice'] = $grouping[0]['totalPrice'];
+		$flightGrouping['cheapestGroup'] = $grouping[0]['uniqueId'];
+
+		return $flightGrouping;
+	}
+
+	private function flightGroupingPattern()
+	{
+		return array(
+			"flights" => "",
+			"groups" => array(),
+			"totalGroups" => 0,
+			"totalFlights" => 0,
+			"cheapestPrice" => 0,
+			"cheapestGroup" => 0
+		);
+	}
+
+	private function getTotalUsedFlights($grouping)
+	{
+		$usedFlights = array();
+
+		foreach ($grouping as $group) {
+			$outbound = array_map(function($flight) { return $flight['id']; }, $group['outbound']);
+			$inbound = array_map(function($flight) { return $flight['id']; }, $group['inbound']);
+			
+			$usedFlights = array_merge($usedFlights, $outbound, $inbound);
+		}
+
+		$usedFlights = array_unique($usedFlights);
+
+		return count($usedFlights);
+	}
+
 }
